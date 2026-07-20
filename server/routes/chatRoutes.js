f61655dfd399df28e1265ddc70f2153818a2ba41
Bollapelli Sai Teja <119847140+SaiTeja-tech-byte@users.noreply.github.com>
2026-07-20 14:29:11 +0530

    Update chatRoutes.js

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -9,17 +9,25 @@ const {
   getConversations,
   getMessages,
   sendMessage,
+  markConversationRead,
+  editMessage,
+  deleteMessage,
+  getUnreadCount,
 } = require("../controllers/chatController");
 
 router.use(requireAuth);
 
+router.get("/unread-count", getUnreadCount);
 router.post("/request", sendChatRequest);
 router.get("/requests", getIncomingRequests);
 router.post("/request/:id/accept", acceptChatRequest);
 router.post("/request/:id/decline", declineChatRequest);
 
 router.get("/conversations", getConversations);
+router.patch("/conversations/:id/read", markConversationRead);
 router.get("/messages/:conversationId", getMessages);
 router.post("/messages", sendMessage);
+router.patch("/messages/:id", editMessage);
+router.delete("/messages/:id", deleteMessage);
 
 module.exports = router;
