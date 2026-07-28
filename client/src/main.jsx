ae518d11818e712808f83dff71c486d59dbf44aa
Bollapelli Sai Teja <119847140+SaiTeja-tech-byte@users.noreply.github.com>
2026-07-28 12:40:03 +0530

    Remove GoogleOAuthProvider from main.jsx
    Removed GoogleOAuthProvider from the component tree.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 import { CRMProvider } from './context/CRMContext';
 import './index.css';
@@ -9,16 +8,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles/auth.css';
 
-const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "";
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <GoogleOAuthProvider clientId={googleClientId}>
-        <CRMProvider>
-          <App />
-        </CRMProvider>
-      </GoogleOAuthProvider>
+      <CRMProvider>
+        <App />
+      </CRMProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
