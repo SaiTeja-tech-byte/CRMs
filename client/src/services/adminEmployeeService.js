dcf4e857b90cb285370b49d210490a67dc1a0bc5
PILLI KESHAV RAO <keshavraoplacement@gmail.com>
2026-08-05 11:59:30 +0530

    fix: Update adminEmployeeService API_BASE and token key

--- client/src/services/adminEmployeeService.js
+++ client/src/services/adminEmployeeService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = (import.meta.env.VITE_API_URL || "https://crms-1.onrender.com/api").replace(/\/auth\/?$/, "");
 
 const authHeaders = () => {
-  const token = localStorage.getItem("crm_token");
+  const token = localStorage.getItem("token");
   return { headers: { Authorization: `Bearer ${token}` } };
 };
 
