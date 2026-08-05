73b66f46cdffcc077d1dd059e7d377588e554ee1
Bollapelli Sai Teja <119847140+SaiTeja-tech-byte@users.noreply.github.com>
2026-08-05 14:14:37 +0530

    Update reportRoutes.js

--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const requireAuth = require("../middleware/authMiddleware");
-const { getTasks, createTask, updateTask, deleteTask } = require("../controllers/taskController");
+const { getAttendanceReport } = require("../controllers/reportController");
 
 router.use(requireAuth);
-router.get("/", getTasks);
-router.post("/", createTask);
-router.patch("/:id", updateTask);
-router.delete("/:id", deleteTask);
+
+
+router.get("/attendance", getAttendanceReport);
 
 module.exports = router;
